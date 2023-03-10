e543fd7c9622a79619626a826ec8b8354593de1b
박홍빈 <parkhongbin95@gmail.com>
2023-03-10 23:48:50 +0900

    Create Main_17478_박홍빈.java
    20230310 박홍빈

---
+++ 2023/03/10/Main_17478_박홍빈.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_17478_박홍빈 {
+    public static void main(String[] args) throws Exception {
+        int T;
+
+        System.setIn(new FileInputStream("input/bj17478.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for(int t = 1; t <= T; t++){
+            String str = br.readLine();
+
+
+        }
+
+    }   // main
+
+}
