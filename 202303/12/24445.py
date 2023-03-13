bbf92d12e262305f92166edf1b4bcc3660093794
mmy789 <mmy789@naver.com>
2023-03-14 08:06:55 +0900

    20230312 - 2

---
+++ 202303/12/24445.py
@@ -0,0 +1,36 @@
+from sys import stdin
+from collections import deque
+
+
+def bfs(start):
+    global cnt
+    visited[start] = cnt
+    cnt += 1
+    queue = deque([start])
+
+    while queue:
+        now = queue.popleft()
+
+        for node in graph[now]:
+            if visited[node] == 0:
+                visited[node] = cnt
+                cnt += 1
+                queue.append(node)
+
+
+n, m, r = map(int, stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(m):
+    u, v = map(int, stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, n + 1):
+    graph[i].sort(reverse=True)
+
+cnt = 1
+bfs(r)
+for i in range(1, n + 1):
+    print(visited[i])
